0f3fcf67f69c6312aebad82e2aa0bf2243c8d125
puu <puu9000@gmail.com>
2021-02-25 14:38:50 +0200

    Adds constants.h for adjustable values etc. Adds upper temperature limit that triggers defrosting even the minimum heating time is not passed. Sets initial heating passed initial value to true to avoid too long heating period after reboot.

---
+++ constants.h
@@ -0,0 +1,7 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+const float TEMPERATURE_DELTA_TO_DEFROST       = 4.0;
+const float TEMPERATURE_DELTA_TO_FORCE_DEFROST = 8.0;
+
+#endif /* #define __CONSTANTS_H__ */
