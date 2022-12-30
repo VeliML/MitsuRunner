dca5775ef03c237ed79bab1728ca5178dda96fdf
puu <79506378+kestopuu@users.noreply.github.com>
2022-12-30 22:41:30 +0200

    Fixed typos, language, formatting etc. (#27)

--- constants.h
+++ constants.h
@@ -1,26 +1,24 @@
-/*
-# Copyright (c) 2021 Veli Matti Lastumäki (Velsku at lampopumput.info),
-#                    Joonas Ihonen (puu at lampopumput.info)
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/* Copyright (c) 2021 Veli Matti Lastumäki (Velsku at lampopumput.info),
+ *                    Joonas Ihonen (puu at lampopumput.info)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #ifndef __CONSTANTS_H__
 #define __CONSTANTS_H__
@@ -44,22 +42,22 @@ const float HEAT_EXCHANGER_MAX_TEMPERATURE = 50.0;
  * forced defrosting will be started. */
 #define MAX_HEATING_TIME                150 /* minutes */
 
-/* The minumum time between defrosting operations. */
+/* The minimum time between defrosting operations. */
 #define MIN_HEATING_TIME                50  /* minutes */
 
 /* The time that the defrost hack relays is off after defrosting is started. */
 #define RELAY_OFF_TIME                  30  /* minutes */
 
 /* If defrosting is not started during this time after switching the relay off,
-   state will be set back to IDLE inetead of DEFROSTING STARTED */
+   state will be set back to IDLE instead of DEFROSTING STARTED */
 #define DEFROST_TIMEOUT                 10  /* minutes */
 
 /* Delay at the reset before allowing state machine to step to next states 
- * Gives time for the sensors to be reads*/
+ * Gives time for the sensors to be read*/
 #define RESET_SENSOR_DELAY				25  /* seconds */
  
-/* Delay at the device bootup before starting the state machine to give 
- * time for the sensors to be read, system to connect to wifi and system to connect to MQTT broker */
+/* Delay at the device bootup before starting the state machine to give time for the
+ * sensors to be read, system to connect to wifi and system to connect to MQTT broker */
 #define INITIALIZE_DELAY                60  /* seconds*/
 
 
